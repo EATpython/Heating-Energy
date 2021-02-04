a21bbf2840a3fca3ea78a2a414c017dacd5be709
tshoorideh <69776499+tshoorideh@users.noreply.github.com>
2021-02-03 21:32:51 -0800

    Update heat-calc.py

--- heat-calc.py
+++ heat-calc.py
@@ -1,1 +1,24 @@
 #Practice collaboration file
+
+# Step 1 - Lets list all the libraries that needs to be imported
+
+
+# Step 2 - Lets Define all the User Inputs Required
+
+# Step 3 - Lets Define all the variables that we will use with description
+
+
+
+# Step 4 - Lets Create all the functions 
+# 4.a : User Propmt Input, save to 
+# 4.b : Data Cleaning
+# 4.c : Load Profile Generation
+# 4.d : Equipment Demand Function
+# 4.e : Boiler Consumption Function 
+# 4.f : Chiller Consumption Function 
+# 4.g : Electric Cost Calculator Function 
+# 4.h: Gas Cost Calculator FUnction 
+# 4.i : Carbon Calculator Function 
+# 4.j : Variable vs Time Plotter function 
+# 4.k : Variable vs Variable Plotter Function 
+
