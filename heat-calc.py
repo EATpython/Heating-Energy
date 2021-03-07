0422945f424172d770150f11ba097c969f46f41f
maximus-sunshine <37388397+maximus-sunshine@users.noreply.github.com>
2021-03-07 12:43:38 -0800

    Update heat-calc.py
    re-added variable vs. time function. must have accidentally deleted at some point

--- heat-calc.py
+++ heat-calc.py
@@ -533,6 +533,35 @@ writer.save()
 ## Outputs 
 ##############################################################################
 
+##############################################################################
+# 4.j : Variable vs Time Plotter function - @max
+## Inputs
+#
+#   df - a pandas DataFrame object with timestamps in the first column & some variable of interest in the second column.
+#
+## Outputs 
+#
+#   No outputs. This function just draws a plot. We could change it so it returns a matplotlib object (figure or axes) and then use that to plot later.
+#
+## TODO
+#
+#   -Make more flexible/robust
+#   -If it's always going to be 8760 data, we can work on making the x-axis prettier with month names
+#
+def draw_plot(df):
+    y_values = df.iloc[:,1]  # Values in the 2nd column will be plotted on the y-axis
+    x_values = range(len(y_values))  # x-axis is just a range of the same length as y_values
+
+    y_label = df.columns[1]  # Name of 2nd column is y-axis label
+    x_label = df.columns[0]  # Name of 1st column is x-axis label
+
+    fig, ax = plt.subplots()  # Create a figure containing a single axes.
+    ax.set_title(y_label + ' vs. ' + x_label)  # set the title
+    ax.set_xlabel(x_label)  # label the x-axis
+    ax.set_ylabel(y_label)  # label the y-axis
+    ax.plot(x_values, y_values, lw=0.1)  # Plot the data
+    plt.show()  # show the plot
+    return
 
 ##############################################################################
 # 4.k : Variable vs Variable Plotter Function - @max
