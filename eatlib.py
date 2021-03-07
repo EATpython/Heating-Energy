cdb22282f1db982db41189eecdfaa24932fa65dd
maximus-sunshine <maxsun96@gmail.com>
2021-03-07 15:54:10 -0800

    changed plot_time function to use plotly instead of matplotlib.

--- eatlib.py
+++ eatlib.py
@@ -3,12 +3,15 @@
 # eatlib - Energy Automation Team (EAT) Library
 #
 # this is a library of functions to be used in EAT scripts
-#
-####################################################################################################################
 
+
+
+####################################################################################################################
 # IMPORTS
 
 import pandas as pd
+import plotly.express as px
+import datetime
 # import numpy as np
 # import warnings
 # import time
@@ -17,46 +20,77 @@ import pandas as pd
 # from tkinter import filedialog
 # from tkinter import messagebox
 # from scipy.optimize import curve_fit
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 
+
+
+####################################################################################################################
 # FUNCTIONS:
 
-# Plotting Functions:
+# PLOTTING FUNCTIONS:
 
+#####################################################
 # plot_time(df) - variable vs. time plotting function
+#
+#   Imports:
+#
+#   pandas ad pd
+#   from matplotlib import pyplot as plt
 #
 #   Inputs:
 #
 #   df - a pandas DataFrame object with timestamps in the first column & some variable of interest in the second column.
 #
+#
 #   Outputs:
 #
 #   No outputs. This function just draws a plot. We could change it so it returns a matplotlib object (figure or axes) and then use that to plot later.
+#
 #
 #   TODO:
 #
 #   -Make more flexible/robust
 #   -If it's always going to be 8760 data, we can work on making the x-axis prettier with month names
 #
+# PLOTLY VERSION - STABLE
 def plot_time(df):
+    pd.options.plotting.backend = "plotly"  # activate Plotly backend
+    print('PLOTLY BACKEND ACTIVATED')
+
     y_values = df.iloc[:,1]  # Values in the 2nd column will be plotted on the y-axis
     x_values = range(len(y_values))  # x-axis is just a range of the same length as y_values
 
-    x_ticks = [x*796.364 for x in range(12)]    # create x ticks corresponding to months
-    months = ['J','F','M','A','M','J','J','A','S','O','N','D']  # list of months
+    y_label = df.columns[1] # name of 2nd column is y-axis label
+    x_label = df.columns[0] # name of 1st column is x-axis label
+    xy_labels = {'x': x_label, 'y': y_label}    # create a dictionary of the labels to pass to px.line
 
-    y_label = df.columns[1]  # Name of 2nd column is y-axis label
-    x_label = df.columns[0]  # Name of 1st column is x-axis label
-
-    fig, ax = plt.subplots()  # Create a figure containing a single axes.
-    ax.set_title(y_label + ' vs. ' + x_label)  # set the title
-    ax.set_xlabel(x_label)  # label the x-axis
-    ax.set_ylabel(y_label)  # label the y-axis
-    ax.set_xticks(x_ticks)  # set and label x-ticks
-    ax.set_xticklabels(months)
-    ax.plot(x_values, y_values, lw=0.1)  # Plot the data
-    plt.show()  # show the plot
+    fig = px.line(x=x_values, y=y_values, labels=xy_labels, title=y_label + ' vs. ' + x_label)  # plot using plotly
+    fig.show()
     return
+
+# MATPLOTLIB VERSION - STABLE
+# def plot_time(df):
+#     y_values = df.iloc[:,1]  # Values in the 2nd column will be plotted on the y-axis
+#     x_values = range(len(y_values))  # x-axis is just a range of the same length as y_values
+#
+#     x_ticks = [x*796.364 for x in range(12)]    # create x ticks corresponding to months
+#     months = ['J','F','M','A','M','J','J','A','S','O','N','D']  # list of months
+#
+#     y_label = df.columns[1]  # Name of 2nd column is y-axis label
+#     x_label = df.columns[0]  # Name of 1st column is x-axis label
+#
+#     fig, ax = plt.subplots()  # Create a figure containing a single axes.
+#     ax.set_title(y_label + ' vs. ' + x_label)  # set the title
+#     ax.set_xlabel(x_label)  # label the x-axis
+#     ax.set_ylabel(y_label)  # label the y-axis
+#     ax.set_xticks(x_ticks)  # set and label x-ticks
+#     ax.set_xticklabels(months)
+#     ax.plot(x_values, y_values, lw=0.1)  # plot using matplotlib
+#     plt.show()  # show the plot
+#     return
+#####################################################
+
+
 
 # plot_x(df) - variable vs. variable plotting function
 #
--- max.py
+++ max.py
@@ -1,15 +1,21 @@
 ####################################################################################################################
+# max.py
 #
 # script for demonstrating weekly progress - Max
 # imports and calls functions from eatlib
-#
+
+
+
 ####################################################################################################################
-
 # IMPORTS
-from eatlib import *
+from eatlib import * # import eatlib - the only library you'll ever need
 
+
+
+####################################################################################################################
 # SCRIPT
 
+# 001 - call plot_time function from eatlib - STABLE
 root = '/Users/maxsun/EAT' # define path to sample data
 
 df = pd.read_csv(root + '/EquipmentOutput.csv') # read data into a DataFrame and print some info
@@ -20,3 +26,82 @@ print(df.dtypes)
 
 plot_time(df) #call a function from eatlib to plot the data
 
+
+
+
+####################################################################################################################
+# SANDBOX
+
+# 6: use Plotly Express to create entire figures at once. RECOMMENDED APPROACH
+# import pandas as pd
+# pd.options.plotting.backend = "plotly"
+#
+# df = pd.DataFrame(dict(a=[1,3,2], b=[3,2,1]))
+# fig = df.plot()
+# fig.show()
+
+
+
+# # 5: use graph objects figure constructor
+# import plotly.graph_objects as go
+#
+# fig = go.Figure(data=go.Bar(y=[2,3,1]))
+# fig.write_html('first_figure.html', auto_open=True)
+
+
+
+# # 4: use Plotly Express to create entire figures at once. RECOMMENDED APPROACH
+# import plotly.express as px
+#
+# df = px.data.iris()
+# fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species", title="A Plotly Express Figure")
+#
+# # If you print the figure, you'll see that it's just a regular figure with data and layout
+# print(fig)
+#
+# fig.show()
+
+
+
+# # 3: create graph object figure from dictionary representation using go.Figure
+# import plotly.graph_objects as go
+#
+# dict_of_fig = dict({
+#     "data": [{"type": "bar",
+#               "x": [1, 2, 3],
+#               "y": [1, 3, 2]}],
+#     "layout": {"title": {"text": "A Figure Specified By A Graph Object With A Dictionary"}}
+# })
+#
+# fig = go.Figure(dict_of_fig)
+#
+# fig.show()
+
+
+
+# # 2: use a graph object instead of a dictionary
+# import plotly.graph_objects as go
+#
+# fig = go.Figure(
+#     data=[go.Bar(x=[1, 2, 3], y=[1, 3, 2])],
+#     layout=go.Layout(
+#         title=go.layout.Title(text="A Figure Specified By A Graph Object")
+#     )
+# )
+#
+# fig.show()
+
+
+
+# # 1: low level, create figures ground up using dictionaries
+# fig = dict({
+#     "data": [{"type": "bar",
+#               "x": [1, 2, 3],
+#               "y": [1, 3, 2]}],
+#     "layout": {"title": {"text": "A Figure Specified By Python Dictionary"}}
+# })
+#
+# # To display the figure defined by this dict, use the low-level plotly.io.show function
+# import plotly.io as pio
+#
+# pio.show(fig)
