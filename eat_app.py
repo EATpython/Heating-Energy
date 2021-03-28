e130cb9e0f5c8b2717688f95793fc9eef9cb967e
maximus-sunshine <maxsun96@gmail.com>
2021-03-28 16:09:58 -0700

    testing streamlit app

--- eat_app.py
+++ eat_app.py
@@ -19,23 +19,24 @@ df=[]
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     st.write(df)
+    df.drop(df.columns[0], axis=1, inplace=True) # !!! drop the first column of data so timestamps are in column
+    print('calling plot_time function...')
+    fig = plot_time(df)
+
+    st.title('Trend Data Visualization')
+    st.dataframe(df)
+    st.plotly_chart(fig, use_container_width=True)
 # else:
 #     filepath_in = './DataCleaner Output Files/' # use output files from JH_CSV_DataCleaner
 #     file_name = '2019 CHP Raw Trend_OUT_Clean_Data' #name of the file w/o .csv suffix (used to conveniently name plots, but maybe not robust)
 #     df = pd.read_csv(filepath_in + file_name + '.csv') # read data into a DataFrame and print some info
+#     print('\nDATA READ SUCCESSFULLY...\n')
+#     print(df)
 
 
-# print('\nDATA READ SUCCESSFULLY...\n')
-# print(df)
-
-# !!! drop the first column of data so timestamps are in column
-df.drop(df.columns[0],axis=1,inplace=True)
 
 # call a function from eatlib to plot the data
-print('calling plot_time function...')
-fig = plot_time(df)
+
 # fig.write_html(filepath_out + file_name + '_Plot.html')   # write the plot to html so it's shareable
 # fig.show()
-st.title('Trend Data Visualization')
-st.dataframe(df)
-st.plotly_chart(fig, use_container_width=True)
+
