0d0ed9cfd31f290a729322c478d1362b531e67fa
maximus-sunshine <maxsun96@gmail.com>
2021-03-30 08:26:27 -0700

    testing

--- eat_app.py
+++ eat_app.py
@@ -28,7 +28,7 @@ fig = go.Figure()
 # Trend Data Visualization App
 Quickly visualize lots of trend data to gain engineering insights and make nice looking graphs to include in reports/presentations.
 
-To get started, upload a .csv file with timestamps in the first column and trend data in the remaining columns or click "See example"
+To get started, upload a .csv file with timestamps in the first column and trend data in the remaining columns or click "See example".
 """
 uploaded_file = st.file_uploader("Choose a file")
 
